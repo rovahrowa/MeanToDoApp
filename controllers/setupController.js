9cb68f9938c4d3ded5501c28109b044d0b118700
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-07 10:24:24 +0300

    Adds the controller for populating initial database

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -10,26 +10,38 @@ module.exports= function (app) {
             var starterTodos = [
 
                 {
-                    Username: "mack",
-                    Todo: "Buy Milk for Mom",
+                    username: "mack",
+                    todo: "Updating Git Project",
                     isDone: true,
                     hasAttachment: false
                 },
                 {
-                    Username: "shem",
-                    Todo: "Buy Milk for Mom",
+                    username: "shem",
+                    todo: "Buy Milk for Mom",
                     isDone: true,
                     hasAttachment: false
                 },
                 {
-                    Username: "danstan",
-                    Todo: "Buy Milk for Mom",
+                    username: "danstan",
+                    todo: "Cloning the betting application",
                     isDone: true,
                     hasAttachment: false
                 },
                 {
-                    Username: "fred",
-                    Todo: "Buy Milk for Mom",
+                    username: "fred",
+                    todo: "Checking Logs",
+                    isDone: true,
+                    hasAttachment: false
+                },
+                {
+                    username: "slow",
+                    todo: "Checking Logs",
+                    isDone: true,
+                    hasAttachment: false
+                },
+                {
+                    username: "danstan",
+                    todo: "Setting up v8 Datacenter",
                     isDone: true,
                     hasAttachment: false
                 }
