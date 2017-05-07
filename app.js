b73a931381b6c0c5c94c87abf802c18a07e03234
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-07 10:23:13 +0300

    calling the cotrollers to create API endpoints

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 /**
  * Created by danstan on 5/7/17.
  */
-var express = require('express');
-
-var app = express();
-
-var mongoose = require('mongoose');
-
-var setupupController = require ('./controllers/setupController');
+var port = process.env.PORT || 8090;
 
 var config = require('./config');
 
-var port = process.env.PORT || 8090;
+//Create Express Server
+var express = require('express');
+var app = express();
 
+//For initial Setup of Database and other things
+var setupController = require ('./controllers/setupController');
+//Todos API Controller
+var todoController = require('./controllers/todoApiController');
+
+//Views Locations and Module
 app.use('/assets', express.static(__dirname + './public'));
-
 app.set('view engine', 'ejs');
 
+
+//Database Connection
+var mongoose = require('mongoose');
 mongoose.connect(config.getTDBConnectionSreing() );
 
-setupupController(app)
+//Controllers
+setupController(app);
+todoController(app);
 
+//Routes
 app.listen(port);
 
 console.log('Server started running on port ' + port);
